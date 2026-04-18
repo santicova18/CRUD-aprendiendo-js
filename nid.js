ace5211e67e4ac26624915784c8cb59b8c7b391b
Santiago <santicova18@gmail.com>
2026-04-17 19:33:36 -0500

    arreglo del js , se añadio la funcion read

--- nid.js
+++ nid.js
@@ -19,12 +19,14 @@ form.addEventListener("submit", function(e){
     let tarea={
     id: contador,
     descripcion: valor,
-    estado: "pendiente"
+    estado: "pendiente",
+     
     
     };
 
 contador++;
 arr.push(tarea);
+mostrarTareas();
 console.log(arr);
 input.value="";
  
@@ -32,21 +34,37 @@ input.value="";
 
 function mostrarTareas() {
     let lista = document.getElementById("lista-tareas");
-    lista.innerHTML = ""; 
+    lista.innerHTML = "";
+
     arr.forEach(function(tarea) {
-        let item = document.createElement("tr");
-        item.textContent =  tarea.id + " - " + tarea.descripcion + " - " + tarea.estado;
-        lista.appendChild(item);
+        let fila = document.createElement("tr");
+        let datos = [tarea.id, tarea.descripcion, tarea.estado ];
+
+        datos.forEach(function(dato) {
+            let celda = document.createElement("td");
+            celda.textContent = dato;
+            fila.appendChild(celda);
+            let accion= document.createElement("button");
+            accion.textContent="Eliminar";
+       
+        });
+        let celdaAcciones = document.createElement("td");
+
+        let botonEliminar = document.createElement("button");
+        botonEliminar.textContent = "Eliminar";
+
+        celdaAcciones.appendChild(botonEliminar);
+        fila.appendChild(celdaAcciones);
+
+        lista.appendChild(fila);
     });
- let datos=[item];
-   arr.forEach(function(tarea) {
-    dato = document.createElement("td");
-    dato.textContent =  tarea.id + " - " + tarea.descripcion + " - " + tarea.estado;
-    listas.appendChild(dato);
-   })
- 
+
+    
 
 }
+ 
+
+
 
 
 
